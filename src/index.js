f3cd05ad6311519a25d74e21313af968b7254d88
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2024-12-16 19:54:26 +0530

    using next in express for corner cases

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const app=express();
 
+
+app.use("/",(req,res,next)=>{
+    
+    console.log("Print response 1");
+    next();
+    //res.send("response 1");
+},
+(req,res,next)=>{
+   // res.send("response 2");
+    console.log("print response 2");
+    next();
+}
+,
+[(req,res,next)=>{
+    //res.send("response 3");
+    console.log("print response 3");
+    next();
+}
+,
+(req,res,next)=>{
+   // res.send("response 4");
+    console.log("print response 4");
+    next();
+}
+,
+(req,res,next)=>{
+    res.send("response 5");
+    console.log("print response 5");
+}]
+)
 app.listen(3000,(req,res)=>{
     console.log("Listening on port is 3000");
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello world");
-})
-
-app.use("/test",(req,res)=>{
-    res.send("This is a test");
-})
-
-app.use("/",(req,res)=>{
-    res.send("Normal Page");
-})
